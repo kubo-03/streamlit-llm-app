ab953e768f2c1e85ef0e324354c9f741711b4ef0
TABLET-VQ37MGBH\choch <okubo_syukatu@yahoo.co.jp>
2025-05-28 23:13:51 +0900

    update

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
+st.title("サンプルアプリ①: 簡単なWebアプリ")
+
+input_message = st.text_input(label="文字数のカウント対象となるテキストを入力してください。")
+
+text_count = len(input_message)
+
+if st.button("実行"):
+
+    st.write(f"文字数: **{text_count}**")
 
 
 
