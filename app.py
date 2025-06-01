5d6fe4312990dbc9b3e564dba21b62aa99bc58fa
TABLET-VQ37MGBH\choch <okubo_syukatu@yahoo.co.jp>
2025-06-01 17:34:28 +0900

    update

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
+from dotenv import load_dotenv
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAI
 
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
 
+load_dotenv()  # .env ファイルから環境変数を読み込む
+
+client = OpenAI()  # .env の OPENAI_API_KEY を使う
+
 llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+#質問を回答するLLM
+
+input_text = st.text_input(label="`質問を入力してください`")
+    
+selected_item = st.radio(
+    "専門家を選択してください",
+    ["健康に関する専門家", "教育に関する専門家"]
+)
 
 messages = [
 
-    SystemMessage(content="You are a helpful assistant."),
+    SystemMessage(content=f"あなたは{selected_item}です。質問に答えてください。"),
 
-    HumanMessage(content="私の名前は大久保雅子です"),
-
-    AIMessage(content="大久保雅子です"),
-
-    HumanMessage(content="私の名前が分かりますか？"),
+    HumanMessage(content=input_text),
 
 ]
 
